7847c15d31cd8aec331d1de501f1aa8323f520ff
Tim Dill <brodt7258@gmail.com>
2018-08-31 01:46:25 -0400

    tweaked Answer styles

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -6,10 +6,12 @@ const StyledAnswer = styled.div`
 	border: thin solid black;
 	margin: 0.5rem auto;
 	width: 60%;
+	height: 2.5rem;
 	border-radius: 0.25rem;
 	font-size: 2rem;
 	background-color: #eee;
 	min-width: 3rem;
+	line-height: 2.5rem;
 `;	
 
 const Answer = ({ children }) => {
